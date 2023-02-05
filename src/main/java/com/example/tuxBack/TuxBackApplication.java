d5308fe024b6ca03b9f1fd6451a216f529d75bfe
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2023-02-06 00:22:04 +0900

    spring project 생성

---
+++ src/main/java/com/example/tuxBack/TuxBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.tuxBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuxBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuxBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tuxBack/TuxBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tuxBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuxBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
